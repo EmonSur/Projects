96977678d982cd4f6a87fd75095d6aff5cdf434f
EmonSur <93683856+EmonSur@users.noreply.github.com>
2024-09-20 11:05:40 +0100

    Add files via upload

---
+++ javascript/ca/level4.py
@@ -0,0 +1,4 @@
+my_array = [[ -1] * 54 for i in range(34)]
+
+
+print(my_array)
---
+++ javascript/ca/levels.js
@@ -0,0 +1,20 @@
+let level1Button = document.getElementById('level-1');
+let level2Button = document.getElementById('level-2');
+let level3Button = document.getElementById('level-3');
+let level4Button = document.getElementById('level-4');
+
+level1Button.addEventListener('click', function() {
+	window.location.href = 'level1.html';
+});
+
+level2Button.addEventListener('click', function() {
+	window.location.href = 'level2.html';
+});
+
+level3Button.addEventListener('click', function() {
+	window.location.href = 'level3.html';
+});
+
+level4Button.addEventListener('click', function() {
+	window.location.href = 'level4.html';
+});
